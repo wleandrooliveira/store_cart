5129b23a165358cdbc67b27694ae302e5075cca0
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2018-11-20 09:53:09 -0200

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates :title, :description, :price_unit, :quantity, presence: true
+    validates :price_unit, numericality: {greater_than_or_equal_to: 0.01}
 end
