238c0972f7037129c2b5da50869a0871332cfab5
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2018-11-20 16:32:08 -0200

    Create Cart - items and line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :title, :description, :price_unit, :quantity, presence: true
     validates :price_unit, numericality: {greater_than_or_equal_to: 0.01}
+
+    private
+
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+            erros.add(:base, 'Line Items present')
+            throw :abort
+        end
+    end
 end
